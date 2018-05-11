ef10236d79d87f815a97693f7d08d4b2f8aff6c3
Woody Lee <woody.lee@EF.com>
2018-05-11 13:32:46 +0800

    remove install section

--- install.sh
+++ install.sh
@@ -29,9 +29,9 @@ echo "✔ oh my zsh configured!"
 ln -sf $(pwd)/gitconfig ~/.gitconfig
 echo "✔ .gitconfig symlinked"
 
-mkdir -p ~/.bin
-ln -sf /mnt/c/Program\ Files/Microsoft\ VS\ Code/code.exe ~/.bin/code
-echo "✔ alias for windows programs"
+# mkdir -p ~/.bin
+# ln -sf /mnt/c/Program\ Files/Microsoft\ VS\ Code/code.exe ~/.bin/code
+# echo "✔ alias for windows programs"
 
 mkdir -p ~/.scripts
 ln -sf $(pwd)/scripts ~/.scripts
@@ -52,17 +52,6 @@ rm -f ~/.m2
 ln -sf /mnt/c/Users/woody.lee/.ssh ~/.m2
 echo "✔ alias for windows folders"
 
-<<<<<<< HEAD
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-sudo apt -qq install nodejs
-sudo apt -qq install npm
-sudo apt -qq install python3-pip
-echo "✔ apt dev tools installed"
-
-# sudo apt -qq install caca-utils ranger cowsay fortune screenfetch lolcat figlet sl cmatrix
-# echo "✔ other cool stuff installed"
-
-=======
 # curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 # sudo apt -qq install nodejs
 # sudo apt -qq install npm
@@ -71,4 +60,3 @@ echo "✔ apt dev tools installed"
 
 # sudo apt -qq install caca-utils ranger cowsay fortune screenfetch lolcat figlet sl cmatrix
 # echo "✔ other cool stuff installed"
->>>>>>> 86ecf5a872e95297e2c3323dd89bda981dd5ea84
